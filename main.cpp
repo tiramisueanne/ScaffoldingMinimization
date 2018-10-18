6cb97a80543b7424db0c8ad6221ed0d543f17f2d
Drothknar <sueanne@cs.utexas.edu>
2018-10-18 15:12:45 -0500

    Added dual through barycenter and connecting adjacent triangles

--- main.cpp
+++ main.cpp
@@ -1,33 +1,69 @@
 #include <igl/opengl/glfw/Viewer.h>
+#include <igl/triangle_triangle_adjacency.h>
+#include <igl/readOBJ.h>
+#include <iostream>
 
-int main(int argc, char *argv[])
-{
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
   // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-    0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    1,7,5,
-    1,3,7,
-    1,4,3,
-    1,2,4,
-    3,8,7,
-    3,4,8,
-    5,7,8,
-    5,8,6,
-    1,5,6,
-    1,6,2,
-    2,6,8,
-    2,8,4).finished().array()-1;
+  Eigen::MatrixXd V = (Eigen::MatrixXd(6, 3) << 0.0, 0.0, 0.0,
+
+                             0.0, 1.0, 0.0,
+
+                             -1.0, -1.0, 0.0,
+
+                             1.0, -1.0, 0.0,
+
+                             1.0, 0.0, 0.0,
+
+                             -1.0, 0.0, 0.0)
+                                .finished();
+  Eigen::MatrixXi F = (Eigen::MatrixXi(5, 3) << 1, 2, 6, /* a */
+
+                             1, 5, 2, /* b */
+
+                             1, 6, 3,
+
+                             1, 3, 4,
+
+                             1, 4, 5)
+                                .finished()
+    .array() - 1;
+
+  igl::readOBJ("./rec.obj", V, F);
+
+  Eigen::MatrixXd BC;
+  igl::barycenter(V, F, BC);
+  Eigen::MatrixXi TT;
+  Eigen::MatrixXi TTi;
+  // Go through and add edges between each BC and the BC it is adjacent to
+  igl::triangle_triangle_adjacency(F, TT, TTi);
+
+  cout << "the outer size of TT is " << TT.rows() << endl;
+  cout << "The inner size of TT is " << TT.cols() << endl;
+  cout << "the rows of barycenter is " << BC.rows() << endl;
+
+
+  igl::opengl::glfw::Viewer viewer;
+  Eigen::RowVector3d color;
+  color << 0,0,200;
+  // Isn't | TT | == | BC |, the number of the triangles?
+  for(int i = 0; i < TT.rows(); i++) {
+    auto barycenteri = BC.row(i);
+    for(int j = 0; j < TT.row(i).size(); j++) {
+      // The index of the triangle adjacent to edge j of triangle i
+      int adjIndex = TT(i, j);
+      if (adjIndex == -1) {
+        continue;
+      }
+      auto barycenterk = BC.row(adjIndex);
+      viewer.data().add_edges(barycenteri, barycenterk, color);
+    }
+  }
 
   // Plot the mesh
-  igl::opengl::glfw::Viewer viewer;
   viewer.data().set_mesh(V, F);
   viewer.data().set_face_based(true);
   viewer.launch();
