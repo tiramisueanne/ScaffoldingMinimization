d2a598fa528416ca241763765f8e80d95bfaf315
Drothknar <sueanne@cs.utexas.edu>
2019-03-03 22:06:55 -0600

    Wrote some of the code for setting up zValues

--- Indexer.h
+++ Indexer.h
@@ -9,12 +9,11 @@ class Indexer {
     map<pair<int, int>, int> edgeIndex;
 
    public:
-    Indexer() : numVert(-1) {};
+    Indexer() : numVert(-1){};
     Indexer(int numVertices, set<int> internalNodes,
             set<pair<int, int>> allEdges);
 
     inline int indexVert(int vertex) { return indexes[vertex]; }
-
 
     // If the edge is v1 - v2
     inline int indexEdge(int v1, int v2) {
@@ -23,10 +22,14 @@ class Indexer {
 
     inline const map<pair<int, int>, int> &edgeMap() { return edgeIndex; }
 
+    #define XDim 0
+    #define YDim 1
+    #define ZDim 2
+    inline int indexBigVert(int vertex, int dim) { return indexes[vertex] * 3 + dim; }
 };
 
 inline Indexer::Indexer(int numVertices, set<int> internalNodes,
-                 set<pair<int, int>> allEdges)
+                        set<pair<int, int>> allEdges)
     : numVert(numVertices), indexes(numVert) {
     int matrixIndex = 0;
     for (int internal : internalNodes) {
--- UpdateVertices.cpp
+++ UpdateVertices.cpp
@@ -14,15 +14,26 @@ double QuadraticSolver::updateVertices() {
     vec = qp::Vector<double>(ZERO, internalNodes.size() * V.cols());
     for (auto row : internalNodes) {
         for (int j = 0; j < V.cols(); j++) {
-            vec[indr.indexVert(row) * V.cols() + j] = V(row, j);
+            vec[indr.indexBigVert(row, j)] = V(row, j);
         }
     }
 
     // Create the new zDiff struct
-    qp::Matrix<double> zValues(internalNodes.size(), edges.size() * V.cols());
+    qp::Matrix<double> zValues(ZERO, internalNodes.size(),
+                               internalNodes.size() * V.cols());
     // Go through each edge and add weights
-    for (pair<pair<int, int>, int> weight : weightMap) {
-        // zValues[indr.indexVert(weight.first.first)][indr.] += weight.second;
-
+    for (const pair<pair<int, int>, double>& edge_weight : weightMap) {
+        const pair<int, int>& edge = edge_weight.first;
+        double weight = edge_weight.second;
+        zValues[indr.indexVert(edge.first)][indr.indexBigVert(edge.first, 2)] +=
+            weight;
+        if (internalNodes.find(edge.second) != internalNodes.end()) {
+            zValues[indr.indexVert(edge.first)]
+                   [indr.indexBigVert(edge.second, ZDim)] -= weight;
+        } else {
+            // Using the X values in the first edge's x place
+            zValues[indr.indexVert(edge.first)]
+                [indr.indexBigVert(edge.first, XDim)] -= weight * V.row(edge.second).z();
+        }
     }
 }
